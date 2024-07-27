66f9500bf9098e01054666e2025736d13d85b71a
Stanyslas250 <stanyslas.contact@gmail.com>
2024-07-27 12:02:00 +0200

    Creation des fichiers de base et initialisation du projet

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 > Todoify is a task management application (TODO List) that allows users to create, read, update and delete tasks in a simple and intuitive way.
 
 ## Technology Stack and Features
-  * ⚡ FastAPI for the Python backend API.
-    * 🧰 SQLModel for the Python SQL database interactions (ORM).
-    * 🔍 Pydantic, used by FastAPI, for the data validation and settings management.
-    * 💾 PostgreSQL as the SQL database.
+
+* ⚡ FastAPI for the Python backend API.
+  * 🧰 SQLModel for the Python SQL database interactions (ORM).
+  * 🔍 Pydantic, used by FastAPI, for the data validation and settings management.
+  * 💾 PostgreSQL as the SQL database.
---
+++ backend/app/models.py
@@ -0,0 +1,109 @@
+from pydantic import EmailStr
+from sqlmodel import Field, Relationship, SQLModel
+
+
+# Shared properties
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    is_active: bool = True
+    is_superuser: bool = False
+    username: str = Field(min_length=3, max_length=255)
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=40)
+
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+    username: str = Field(min_length=3, max_length=255)
+
+
+# Properties to receive via API on update, all are optional
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    password: str | None = Field(default=None, min_length=8, max_length=40)
+
+
+class UserUpdateMe(SQLModel):
+    username: str | None = Field(default=None, max_length=255)
+    email: EmailStr | None = Field(default=None, max_length=255)
+
+
+class UpdatePassword(SQLModel):
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
+    tasks: list["Task"] = Relationship(back_populates="owner")
+
+
+# Properties to return via API, id is always required
+class UserPublic(UserBase):
+    id: int
+
+
+class UsersPublic(SQLModel):
+    data: list[UserPublic]
+    count: int
+
+
+# Shared properties
+class TaskBase(SQLModel):
+    title: str = Field(min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+
+
+# Properties to receive on Task creation
+class TaskCreate(TaskBase):
+    title: str = Field(min_length=1, max_length=255)
+
+
+# Properties to receive on Task update
+class TaskUpdate(TaskBase):
+    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
+
+
+# Database model, database table inferred from class name
+class Task(TaskBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int | None = Field(default=None, foreign_key="user.id", nullable=False)
+    owner: User | None = Relationship(back_populates="Tasks")
+
+
+# Properties to return via API, id is always required
+class TaskPublic(TaskBase):
+    id: int
+    owner_id: int
+
+
+class TasksPublic(SQLModel):
+    data: list[TaskPublic]
+    count: int
+
+
+# Generic message
+class Message(SQLModel):
+    message: str
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: int | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
