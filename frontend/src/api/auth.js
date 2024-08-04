3288ac57ff7eb3ae5d885b0168eabcdf52a27f47
Stanyslas250 <stanyslas.contact@gmail.com>
2024-08-04 22:56:23 +0200

    Configuring the Login/Register pages

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,49 @@
+const API_URL = "http://localhost:8000"; // Remplacez par l'URL de votre API
+
+const toUrlEncoded = (obj) => {
+  return Object.keys(obj)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]))
+    .join("&");
+};
+
+export const login = async (username, password) => {
+  const response = await fetch(`${API_URL}/api/v1/login/access-token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: toUrlEncoded({ username, password }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    localStorage.setItem("token", data.access_token);
+  } else {
+    throw new Error(data.detail || "Login failed");
+  }
+  return data;
+};
+
+export const signup = async (username, email, password) => {
+  const response = await fetch(`${API_URL}/api/v1/users/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    localStorage.setItem("token", data.access_token);
+  } else {
+    throw new Error(data.detail || "Signup failed");
+  }
+  return data;
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./components/Login";
+import Signin from "./components/Signin";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
