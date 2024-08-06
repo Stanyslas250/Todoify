43dc611fd2020b1fa21709244fcad8b87ce71598
Stanyslas250 <stanyslas.contact@gmail.com>
2024-08-07 00:29:09 +0200

    Design de la login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../api/auth";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
+import BG2 from "../assets/images/bg.jpg";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,41 +23,83 @@ function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-3 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold text-center">Login</h1>
-        {error && <p className="text-red-500">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block mb-1">Email</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-1">Password</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring">
-            Login
-          </button>
-        </form>
-        <p className="text-center">
-          Don't have an account?{" "}
-          <Link to="/signin" className="text-blue-500 hover:underline">
-            Signin
+    <div className="flex items-center w-full h-screen">
+      {/* Image de fond */}
+      <div className="relative h-full flex w-1/2">
+        <img
+          src={BG2}
+          className="w-full h-full object-cover carousel-item after"
+        />
+      </div>
+      {/* Formulaire de connexion */}
+      <div className="bg-base-100 w-2/3 justify-center items-center flex flex-col gap-8">
+        <div className="flex flex-col gap-1 items-center">
+          <h1 className="font-black text-2xl">Welcome back! </h1>
+          <h6 className="text-sm font-semibold">
+            Organise your life in a few clicks
+          </h6>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input
+                type="text"
+                className="grow"
+                placeholder="you@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input
+                type={show ? "text" : "password"}
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label className="swap">
+                <input type="checkbox" onClick={handleClick} />
+                <div className="swap-on">
+                  <i class="bi bi-eye-slash-fill"></i>
+                </div>
+                <div className="swap-off">
+                  <i class="bi bi-eye-fill"></i>
+                </div>
+              </label>
+            </label>
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </form>
+        </div>
+        <p className="text-center flex flex-row gap-2">
+          Don't you have a account?
+          <Link
+            to="/signin"
+            className="text-primary hover:underline hover:font-bold">
+            Sign Up
           </Link>
         </p>
       </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,26 +8,8 @@ export default {
 
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: [
-      "corporate",
-      {
-        light: {
-          primary: "#2f27ce",
-          secondary: "#dddbff",
-          accent: "#443dff",
-          neutral: "#0f0f3d",
-          "base-100": "#fbfbfe",
-        },
-        dark: {
-          primary: "#3a31d8",
-          secondary: "#020024",
-          accent: "#0600c2",
-          neutral: "#0f0f3d",
-          "base-100": "#010104",
-        },
-      },
-    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "corporate", // name of one of the included themes for dark mode
+    themes: ["nord", "dark", "business"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "business", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
