dda5cba9c3bcaa4cc3d8c502d62257549dd7d7eb
Stanyslas250 <stanyslas.contact@gmail.com>
2024-08-10 19:46:58 +0200

    Some changes to the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ const categories = [];
 
 const App = () => {
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 min-h-screen">
       <Navbar />
       <h1 className="text-3xl font-bold text-center mb-6">Categories</h1>
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,10 +6,12 @@ function Navbar() {
   const navigate = useNavigate();
 
   // Toogle theme
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState();
   const toggleTheme = () => {
-    setTheme(theme === "business" ? "nord" : "business");
+    setTheme(theme === "nord" ? "business" : "nord");
   };
+
+  const name = "Stanyslas Moubiligui"; // Remplacer par le nom de l'utilisateur
 
   useEffect(() => {
     document.querySelector("html").setAttribute("data-theme", theme);
@@ -21,26 +23,30 @@ function Navbar() {
   };
 
   return (
-    <div className="justify-center w-full mx-3 shadow-sm shadow-current mb-2">
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
+    <div className="w-full shadow-sm shadow-current px-8 bg-base-100">
+      <div className="navbar">
+        <div className="navbar-start">
           <a className="no-animation text-xl font-bold">Todoify</a>
         </div>
-        <div className="flex-none gap-2">
+        <div className="navbar-center gap-2">
+          Welcome Back, <span className="font-bold italic">{name}</span>
+        </div>
+        <div className="navbar-end gap-2">
+          <div className="divider divider-horizontal"></div>
           <label className="swap swap-rotate btn btn-square border-2 border-primary-content rounded-lg">
             <input
               type="checkbox"
               className="theme-controller"
               onClick={toggleTheme}
             />
-            <i className="bi bi-moon swap-off text-lg"></i>
-            <i className="bi bi-sun swap-on text-lg"></i>
+            <i className="bi bi-moon swap-off text-sm"></i>
+            <i className="bi bi-sun swap-on text-sm"></i>
           </label>
           <button
             className="btn btn-square border-2 border-primary-content rounded-lg"
             onClick={onLogout}
           >
-            <i className="bi bi-box-arrow-right text-lg"></i>
+            <i className="bi bi-box-arrow-right text-xs"></i>
           </button>
         </div>
       </div>
