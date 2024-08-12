def63c93e062a497b3351cdab71fe10ed79640ed
Stanyslas250 <stanyslas.contact@gmail.com>
2024-08-12 18:48:01 +0200

    Initailisation du projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,35 @@
-import React, { Suspense } from "react";
-import Category from "./pages/Category";
-import Navbar from "./components/Navbar";
-import { Await, useLoaderData } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const { username } = useLoaderData();
-  console.log(username);
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="bg-base-100 min-h-screen">
-      <Suspense
-        fallback={<span className="loading loading-ring loading-lg"></span>}
-      >
-        <Await resolve={username}>{() => <Navbar username={username} />}</Await>
-      </Suspense>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-      <h1 className="text-3xl font-bold text-center my-6">Categories</h1>
-      <Category className="px-8" />
-    </div>
-  );
-};
-
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,63 +1,10 @@
-import * as React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, defer, RouterProvider } from "react-router-dom";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { json } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import WelcomePage from "./components/WelcomePage";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import PrivateRoute from "./components/PrivateRoute";
-import App from "./App";
-import { getUserName } from "./api/auth";
-import { SubCategory } from "./pages/Sub-Category";
-import { loadCategoryById } from "./api/categorie";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <WelcomePage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/home",
-    element: (
-      <PrivateRoute>
-        <App />
-      </PrivateRoute>
-    ),
-    loader: async () => {
-      const username = await getUserName().then((r) => r);
-      return defer({ username });
-    },
-  },
-  {
-    path: "home/subcategory/:id",
-    element: <SubCategory />,
-    loader: async ({ params }) => {
-      const subcategory = await loadCategoryById(params.id);
-      console.log(subcategory.name);
-      return subcategory.name;
-    },
-  },
-]);
-
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
