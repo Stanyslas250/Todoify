6affe83d122194ad1b283024a5de7d4c773e41b4
Stanyslas250 <stanyslas.contact@gmail.com>
2024-08-18 22:16:04 +0200

    Adding different routes to the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+import WelcomeHome from "./views/WelcomeHome.jsx";
+import Home from "./views/Home.jsx";
 import { Toaster } from "react-hot-toast";
+
+const router = createBrowserRouter([
+  { path: "/", element: <WelcomeHome /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/app", element: <Home /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <Toaster />
   </StrictMode>
 );
