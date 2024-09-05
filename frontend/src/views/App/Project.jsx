e5e3ce1655838b254b88d0cd8dbf6a080a4fd23f
Stanyslas250 <stanyslas.contact@gmail.com>
2024-09-05 14:06:00 +0200

    📦 NEW:
    feat: Add project view toggle and dynamic layout
    
    Introduce a new feature allowing users to switch between grid and list views for projects. This enhancement improves user experience by providing flexible layout options.
    
    Key changes:
    - Create ProjectViews component for rendering projects in grid or list format
    - Implement ViewsChoose component for toggling between view modes
    - Update Project view to incorporate new components and dynamic layout
    - Utilize useCategory hook for accurate project count display
    
    This change enhances UI flexibility and user control over project presentation.

--- frontend/src/views/App/Project.jsx
+++ frontend/src/views/App/Project.jsx
@@ -1,24 +1,29 @@
-import { LuFolderArchive } from "react-icons/lu";
 import TopbarSearch from "../../components/App/TopbarSearch";
+import ProjectViews from "../../components/App/ProjectViews/ProjectViews";
+import ProjectList from "../../components/App/UI/ProjectList";
+import ViewsChoose from "../../components/App/UI/ViewsChoose";
+import { useState } from "react";
+import { useCategory } from "../../hooks/useCategory";
 
 export default function Project() {
+  const [isGridView, setIsGridView] = useState(false);
+  const handleToggleView = () => setIsGridView(!isGridView);
+
+  const { countCategories } = useCategory();
   return (
     <div className="flex flex-col">
       <TopbarSearch />
       <div className="flex flex-row justify-between pt-10">
-        <h3 className="text-xl font-semibold">10 Projects</h3>
+        <h3 className="text-xl font-semibold">{countCategories} Projects</h3>
+        <ViewsChoose isSwitch={!isGridView} onClick={handleToggleView} />
       </div>
+
       <div>
         <div className="divider" />
         <div>
-          <div className="flex flex-row items-center gap-2">
-            <LuFolderArchive />
-            <h6>Project Name</h6>
-          </div>
-          <div>
-            <p>Number of task - Tasks</p>
-            <span>On Process</span>
-          </div>
+          <ProjectViews isGridView={isGridView}>
+            <ProjectList />
+          </ProjectViews>
         </div>
       </div>
     </div>
